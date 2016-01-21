e0ee35ddfd2c82dbb30b09465c794bbab8621be3
JGaetjens <jeff.gaetjens@roadrunner.com>
2016-01-21 13:20:22 -0500

    fixed dependency issue

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import isEqual from 'lodash.isequal';
+import isequal from 'lodash.isequal';
 
 export default function Pure() {
   return function decorator(Component) {
     return class PureComponent extends Component {
       shouldComponentUpdate(nextProps, nextState) {
-        return (!this.props ||isEqual(this.props, nextProps)) || (!this.state || isEqual(this.state, nextState));
+        return (!this.props ||isequal(this.props, nextProps)) || (!this.state || isequal(this.state, nextState));
       }
     };
   };
